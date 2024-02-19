95a88501b81cee8473d7aafa8707281ca66a7f58
Davi Taveira <davimtaveira@gmail.com>
2024-02-19 11:10:34 -0300

    Simple CSS changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="w-full flex flex-col justify-center items-center py-10 gap-5">
-            <div className="w-[90%] border-b-2 border-black">Contact</div>
+            <div className="w-[90%] border-b-2 border-black text-5xl py-3">Contact</div>
             <section className="flex md:flex-row flex-col md:justify-evenly w-[90%] my-10 md:gap-0 gap-10">
                 <div className="flex flex-col items-center gap-5">
                     <h3>Social</h3>
