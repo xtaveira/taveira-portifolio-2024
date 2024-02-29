3ec614a91254f461f48574ed04b5ad424e012b87
Davi Taveira <davimtaveira@gmail.com>
2024-02-28 23:46:33 -0300

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,22 +7,24 @@ const Projects = () => {
       <div className="w-[90%] border-b-2 border-black text-5xl py-3">
         Projects
       </div>
-      <div className="w-[90%] border-black border-2 rounded-lg h-[500px] flex flex-col justify-center items-evenly mt-10">
-        <div className="w-[90%]">
-            Container Projetos
+      <div className="w-[90%]  rounded-lg h-[500px] flex flex-col justify-between items-center  mt-10 py-10">
+        <div className="w-[90%] h-full flex justify-center">
+          <div className="w-[70%] border-2 border-red-500 h-[80%] flex justify-between flex-col items-center py-10">
+            <div>image</div>
+            <h4>project title</h4>
+          </div>
         </div>
         <Slider
           aria-label="Temperature"
-          defaultValue={30}
-        //   getAriaValueText={}
-        //   valueLabelDisplay="auto"
-          shiftStep={30}
-          step={10}
+          defaultValue={1}
+          step={1}
           marks
-          min={10}
-          max={110}
+          min={1}
+          max={5}
+          sx={{
+            color: "black",
+          }}
         />
-        <Slider defaultValue={30} step={10} marks min={10} max={110} disabled />
       </div>
     </section>
   );
