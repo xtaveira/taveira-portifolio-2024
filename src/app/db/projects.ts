5d9951161e2013350513b2b70ef62d3ebf12e4da
Davi Taveira <davimtaveira@gmail.com>
2024-03-02 15:35:18 -0300

    updating slider

---
+++ src/app/db/projects.ts
@@ -0,0 +1,34 @@
+const projects = [
+  {
+    id: 1,
+    title: "Monitoramento de Consumo",
+    description: "Site que faz os cálculos do consumo de veículos.",
+    src: "/projects/1.png",
+  },
+  {
+    id: 2,
+    title: "Promptopia",
+    description: "Site que faz os cálculos do consumo de veículos.",
+    src: "/projects/1.png",
+  },
+  {
+    id: 3,
+    title: "Nike Store Landing Page",
+    description: "Site que faz os cálculos do consumo de veículos.",
+    src: "/projects/1.png",
+  },
+  {
+    id: 4,
+    title: "Metro SP Landing Page",
+    description: "Site que faz os cálculos do consumo de veículos.",
+    src: "/projects/1.png",
+  },
+  {
+    id: 5,
+    title: "List for Copy to Clipboard",
+    description: "Site que faz os cálculos do consumo de veículos.",
+    src: "/projects/1.png",
+  },
+];
+
+export default projects;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,30 +1,60 @@
+"use client"
+
+import projects from "@/app/db/projects";
 import { Slider } from "@mui/material";
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
+
+
 
 const Projects = () => {
+
+
+  const [project, setProject] = useState(0)
+
   return (
-    <section className="w-full flex flex-col justify-center items-center py-10 gap-5 bg-[#d9d9d9]">
+    <section className=" w-full flex flex-col justify-center items-center py-10 gap-5 bg-[#d9d9d9]">
       <div className="w-[90%] border-b-2 border-black text-5xl py-3">
         Projects
       </div>
-      <div className="w-[90%]  rounded-lg h-[500px] flex flex-col justify-between items-center  mt-10 py-10">
-        <div className="w-[90%] h-full flex justify-center">
-          <div className="w-[70%] border-2 border-red-500 h-[80%] flex justify-between flex-col items-center py-10">
-            <div>image</div>
-            <h4>project title</h4>
+      <div className="w-[90%]  rounded-lg flex flex-col justify-between items-center mb-5">
+        <div className="w-[90%] h-full flex justify-center  my-4">
+          <div className="w-[70%]  h-[100%] flex justify-between flex-col items-center my-10 gap-5 cardProject">
+            <div className="flex justify-center items-center  w-[90%] h-[100%]  ">
+              <Image className="w-[50%] h-auto rounded-lg"
+                src={projects[project].src}
+                width={400}
+                height={400}
+                alt="Imagem do Projeto"
+              />
+            </div>
+            <div className="items-center  w-full justify-center h-full flex flex-col gap-3">
+              <h2 className="font-bold text-lg">{projects[project].title}</h2>
+              <p>{projects[project].description}</p>
+            </div>
           </div>
         </div>
-        <Slider
-          aria-label="Temperature"
-          defaultValue={1}
-          step={1}
-          marks
-          min={1}
-          max={5}
-          sx={{
-            color: "black",
-          }}
-        />
+        <div className="w-[50%]">
+
+          <Slider
+            aria-label="Temperature"
+            defaultValue={1}
+            step={1}
+            marks
+            min={1}
+            max={5}
+            sx={{
+              color: "black",
+            }}
+            onChange={(e) => {
+              const target = e.target as any;
+              if (target.value != null) {
+                console.log(target.value);
+                setProject(target.value - 1);
+              }
+            }}
+          />
+        </div>
       </div>
     </section>
   );
