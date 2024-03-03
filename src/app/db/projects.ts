952ed27aa5e3857ec933c96053d104d996d542b4
Davi Taveira <davimtaveira@gmail.com>
2024-03-03 14:56:51 -0300

    some updates

--- src/app/db/projects.ts
+++ src/app/db/projects.ts
@@ -8,26 +8,29 @@ const projects = [
   {
     id: 2,
     title: "Promptopia",
-    description: "Site que faz os cálculos do consumo de veículos.",
-    src: "/projects/1.png",
+    description:
+      "Aplicação desenvolvida para salvar prompts para IA, usando Login Google.",
+    src: "/projects/2.png",
   },
   {
     id: 3,
     title: "Nike Store Landing Page",
-    description: "Site que faz os cálculos do consumo de veículos.",
-    src: "/projects/1.png",
+    description: "Landing Page desenvolvida com tema em produtos da Nike.",
+    src: "/projects/3.png",
   },
   {
     id: 4,
     title: "Metro SP Landing Page",
-    description: "Site que faz os cálculos do consumo de veículos.",
-    src: "/projects/1.png",
+    description:
+      "Landing Page desenvolvida para informações sobre o Metro de São Paulo.",
+    src: "/projects/4.png",
   },
   {
     id: 5,
     title: "List for Copy to Clipboard",
-    description: "Site que faz os cálculos do consumo de veículos.",
-    src: "/projects/1.png",
+    description:
+      "Aplicação desenvolvida para facilmente copiar mensagens prontas.",
+    src: "/projects/5.png",
   },
 ];
 
