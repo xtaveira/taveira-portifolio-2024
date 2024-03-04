533fd5078359dba6f6b36f544c5335cdab44c08d
Davi Taveira <davimtaveira@gmail.com>
2024-03-03 22:56:50 -0300

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,7 +10,7 @@ import React, { useState } from "react";
 const Projects = () => {
 
 
-  const [project, setProject] = useState(0)
+  const [project, setProject] = useState(1)
 
   return (
     <section className=" w-full flex flex-col justify-center items-center py-10 gap-5 bg-[#d9d9d9]">
@@ -22,15 +22,15 @@ const Projects = () => {
           <div className="w-[90%]  h-[100%] flex justify-between flex-col items-center my-10 gap-5 cardProject">
             <div className="flex justify-center items-center  w-[100%] h-[100%] ">
               <Image className="sm:w-[70%] xl:w-[40%] lg:w-[60%]  w-[100%]  h-[400px] rounded-lg"
-                src={projects[project].src}
+                src={projects[project - 1].src}
                 width={400}
                 height={400}
                 alt="Imagem do Projeto"
               />
             </div>
             <div className="items-center text-center  w-full justify-center h-full flex flex-col gap-3">
-              <h2 className="font-bold text-lg">{projects[project].title}</h2>
-              <p>{projects[project].description}</p>
+              <h2 className="font-bold text-lg">{projects[project - 1].title}</h2>
+              <p>{projects[project - 1].description}</p>
             </div>
           </div>
         </div>
@@ -46,11 +46,12 @@ const Projects = () => {
             sx={{
               color: "black",
             }}
+            value={project}
             onChange={(e) => {
               const target = e.target as any;
               if (target.value != null) {
                 console.log(target.value);
-                setProject(target.value - 1);
+                setProject(target.value);
               }
             }}
           />
